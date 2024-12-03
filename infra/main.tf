f372d1de598478671fba1e9ce7c7c84b78a80d36
unknown <Emanuel>
2024-12-03 16:18:09 -0500

    update

--- infra/main.tf
+++ infra/main.tf
@@ -9,15 +9,15 @@ resource "aws_security_group" "mc_vpc_security_group" {
     ingress {
         from_port   = var.MINECRAFT_SERVER_PORT
         to_port     = var.MINECRAFT_SERVER_PORT
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        protocol    = var.SECURITY_GROUP_PROTOCOL
+        cidr_blocks = var.VPC_SECURITY_GROUP_CIDR_BLOCKS
     }
 
     egress {
         from_port        = var.MINECRAFT_SERVER_PORT
         to_port          = var.MINECRAFT_SERVER_PORT
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
+        protocol         = var.SECURITY_GROUP_PROTOCOL
+        cidr_blocks      = var.VPC_SECURITY_GROUP_CIDR_BLOCKS
     }
 }
 
@@ -82,7 +82,7 @@ resource "aws_lambda_function" "vpc_logs_monitoring_lambda" {
 
 // Lambda role to monitor VPC
 resource "aws_iam_role" "mc_vpc_monitoring_lambda" {
-  name               = "mc_vpc_monitoring_lambda"
+  name               = var.VPC_MONITORING_LAMBDA_ROLE_NAME
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -111,28 +111,37 @@ resource "aws_iam_policy" "lambda_vpc_monitoring_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action   = var.LAMBDA_PERMISSIONS
+        Action   = var.LAMBDA_EXECUTE_PERMISSIONS
         Effect   = "Allow"
         Resource = "*"
-      }
+      },
+      {
+      Effect = "Allow",
+      Action = var.LAMBDA_LOGGING_PERMISSIONS,
+      Resource = var.LOGS_ARN_WILDCARD
+    }
     ]
   })
 }
 
+resource "aws_cloudwatch_log_group" "vpc_monitoring_lambda_log_group" {
+  name              = var.LOGS_GROUP_PATH
+  retention_in_days = 14
+}
+
 resource "aws_cloudwatch_event_rule" "vpc_monitoring_rule" {
-  name                = "vpc-active-connections-monitoring-lambda-rule"
-  schedule_expression = "rate(5 minutes)"
+  name                = var.MONITORING_LAMBDA_RUN_FREQUENCY_RULE_NAME
+  schedule_expression = var.MONITORING_LAMBDA_RUN_FREQUENCY
 }
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
   rule      = aws_cloudwatch_event_rule.vpc_monitoring_rule.name
-  target_id = "lambda-target"
   arn       = aws_lambda_function.vpc_logs_monitoring_lambda.arn
 }
 
 resource "aws_lambda_permission" "allow_eventbridge_invoke" {
-  action        = "lambda:InvokeFunction"
-  principal     = "events.amazonaws.com"
+  action        = var.EVENTBRIDGE_ACTION_TO_INVOKE_LAMBDA
+  principal     = var.EVENTBRIDGE_PRINCIPAL_URL
   function_name = aws_lambda_function.vpc_logs_monitoring_lambda.function_name
   source_arn    = aws_cloudwatch_event_rule.vpc_monitoring_rule.arn
 }
